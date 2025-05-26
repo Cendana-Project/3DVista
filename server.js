b1cb8897fecda869fb5dc63f36426060a90b7038
@raditya <radityaaydin@gmail.com>
2025-05-26 17:20:09 +0700

    Refactor root route to serve index file directly, removing unnecessary redirect

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const path = require('path');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public/Vista')));
 
-app.get('/kijb-2025', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/Vista/index.htm'));
-});
-
 app.get('/', (req, res) => {
-    res.redirect('/kijb-2025');
+    res.sendFile(path.join(__dirname, 'public/Vista/index.htm'));
 });
 
 const PORT = process.env.PORT || 3000;
